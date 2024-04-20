cfdfaa290ba16f5f214a6939c13cdcc1ba21a764
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-04-20 13:29:47 +0300

    changes

--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,42 +1,63 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { TeacherDto } from './teacher.dto';
 import { TeachersService } from './teachers.service';
 import { TeacherLoginDto } from './teacherLogin.dto';
 
 @Controller('teachers')
 export class TeachersController {
-   
-    constructor(private readonly teacherService: TeachersService) {}
+  constructor(private readonly teacherService: TeachersService) {}
 
-    @Post()
-    async createTeacher(@Body() dto: TeacherDto) {
-      return this.teacherService.createTeacher(dto);
+  @Post()
+  async createTeacher(@Body() dto: TeacherDto) {
+    return this.teacherService.createTeacher(dto);
+  }
+
+  @Get()
+  async getAllTeachers() {
+    return this.teacherService.getAllTeachers();
+  }
+
+  @Get(':id')
+  async getTeacherById(@Param('id') id: string) {
+    const numericId = parseInt(id, 10);
+    return this.teacherService.getTeacherById(numericId);
+  }
+
+  @Put(':id')
+  async updateTeacher(@Param('id') id: string, @Body() dto: TeacherDto) {
+    const numericId = parseInt(id, 10);
+    return this.teacherService.updateTeacher(numericId, dto);
+  }
+
+  @Delete(':id')
+  async deleteTeacher(@Param('id') id: string) {
+    const numericId = parseInt(id, 10);
+    return this.teacherService.deleteTeacher(numericId);
+  }
+
+  @Post('signin')
+  async signIn(@Body() dto: TeacherLoginDto) {
+    return this.teacherService.signIn(dto);
+  }
+
+  @Get('profile')
+  async getTeacherProfile(@Query('token') token: string) {
+    console.log(' token goten', token);
+    if (!token) {
+      throw new UnauthorizedException(
+        'Token is missing in the query parameters',
+      );
     }
-  
-    @Get()
-    async getAllTeachers() {
-      return this.teacherService.getAllTeachers();
-    }
-  
-    @Get(':id')
-    async getTeacherById(@Param('id') id: string) {
-      const numericId = parseInt(id, 10);
-      return this.teacherService.getTeacherById(numericId);
-    }
-  
-    @Put(':id')
-    async updateTeacher(@Param('id') id: string, @Body() dto: TeacherDto) {
-      const numericId = parseInt(id, 10);
-      return this.teacherService.updateTeacher(numericId, dto);
-    }
-  
-    @Delete(':id')
-    async deleteTeacher(@Param('id') id: string) {
-      const numericId = parseInt(id, 10);
-      return this.teacherService.deleteTeacher(numericId);
-    }
-    @Post('signin')
-    async signIn(@Body() dto:TeacherLoginDto ) {
-      return this.teacherService.signIn(dto);
-    }
+    return this.teacherService.getTeacherByToken(token);
+  }
 }
--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TeacherDto } from './teacher.dto';
 import { TeacherLoginDto } from './teacherLogin.dto';
@@ -6,110 +10,139 @@ import * as argon from 'argon2';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 
-
 @Injectable()
 export class TeachersService {
-    constructor(private readonly prisma: PrismaService,    private jwt: JwtService,
-      private config: ConfigService,
-    ) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-    async createTeacher(dto: TeacherDto) {
-      const hash = await argon.hash(dto.password);
-      const teacher = await this.prisma.teacher.create({
-        data: {
-          firstName: dto.firstName,
-          lastName: dto.lastName,
-          otherName: dto.otherName,
-          email: dto.email,
-          password: hash,
-          gender: dto.gender,
-        },
-      });
-  
-      return teacher;
-    }
-  
-    async getAllTeachers() {
-      return this.prisma.teacher.findMany();
-    }
-    
-  
-    async getTeacherById(id: number) {
-      const teacher = await this.prisma.teacher.findUnique({
-        where: { id },
-      });
-  
-      if (!teacher) {
-        throw new NotFoundException(`Teacher with ID ${id} not found`);
-      }
-  
-      return teacher;
-    }
-  
-    async updateTeacher(id: number, dto: TeacherDto) {
-      const existingTeacher = await this.prisma.teacher.findUnique({
-        where: { id },
-      });
-  
-      if (!existingTeacher) {
-        throw new NotFoundException(`Teacher with ID ${id} not found`);
-      }
-  
-      return this.prisma.teacher.update({
-        where: { id },
-        data: dto,
-      });
-    }
-  
-    async deleteTeacher(id: number) {
-      const existingTeacher = await this.prisma.teacher.findUnique({
-        where: { id },
-      });
-  
-      if (!existingTeacher) {
-        throw new NotFoundException(`Teacher with ID ${id} not found`);
-      }
-  
-      return this.prisma.teacher.delete({
-        where: { id },
-      });
+  async createTeacher(dto: TeacherDto) {
+    const hash = await argon.hash(dto.password);
+    const teacher = await this.prisma.teacher.create({
+      data: {
+        firstName: dto.firstName,
+        lastName: dto.lastName,
+        otherName: dto.otherName,
+        email: dto.email,
+        password: hash,
+        gender: dto.gender,
+      },
+    });
+
+    return teacher;
+  }
+
+  async getAllTeachers() {
+    return this.prisma.teacher.findMany();
+  }
+
+  async getTeacherById(id: number) {
+    const teacher = await this.prisma.teacher.findUnique({
+      where: { id },
+    });
+
+    if (!teacher) {
+      throw new NotFoundException(`Teacher with ID ${id} not found`);
     }
 
-    async signIn(dto: TeacherLoginDto) {
-      const management = await this.prisma.teacher.findUnique({
-        where: {
-          email: dto.email
-        }
-      });
-  
-      if (!management) {
-        throw new UnauthorizedException("Invalid credentials");
-      }
-  
-      const pwMatches = await argon.verify(management.password, dto.password);
-  
-      if (!pwMatches) {
-        throw new UnauthorizedException("Invalid credentials");
-      }
-  
-      return this.signToken(management.id, management.email);
+    return teacher;
+  }
+
+  async updateTeacher(id: number, dto: TeacherDto) {
+    const existingTeacher = await this.prisma.teacher.findUnique({
+      where: { id },
+    });
+
+    if (!existingTeacher) {
+      throw new NotFoundException(`Teacher with ID ${id} not found`);
     }
-  
-    async signToken(managementId: number, email: string): Promise<{ access_token: string }> {
-      const payload = {
-        sub: managementId,
-        email,
-      };
-  
-      const secret = this.config.get('JWT_SECRET');
-  
-      const token = await this.jwt.signAsync(payload, {
-        expiresIn: '60m',
-        secret: secret,
-      });
-  
-      return {
-        access_token: token,
-      };
+
+    return this.prisma.teacher.update({
+      where: { id },
+      data: dto,
+    });
+  }
+
+  async deleteTeacher(id: number) {
+    const existingTeacher = await this.prisma.teacher.findUnique({
+      where: { id },
+    });
+
+    if (!existingTeacher) {
+      throw new NotFoundException(`Teacher with ID ${id} not found`);
     }
+
+    return this.prisma.teacher.delete({
+      where: { id },
+    });
+  }
+
+  async signIn(dto: TeacherLoginDto) {
+    const management = await this.prisma.teacher.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!management) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const pwMatches = await argon.verify(management.password, dto.password);
+
+    if (!pwMatches) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return this.signToken(management.id, management.email);
+  }
+
+  async signToken(
+    managementId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: managementId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '60m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+
+  async getTeacherByToken(token: string) {
+    console.log('Received Token', token);
+    // Decode token to extract payload
+    const decodedToken: any = this.jwt.decode(token);
+
+    // Log the decoded token
+    console.log('Decoded Token', decodedToken);
+
+    // Extract teacher ID from decoded token and convert it to a number
+    const teacherId = parseInt(decodedToken.sub, 10);
+
+    // Log the extracted teacher ID
+    console.log('Teacher ID', teacherId);
+
+    // Fetch teacher details using the extracted ID
+    const teacher = await this.prisma.teacher.findUnique({
+      where: { id: teacherId },
+    });
+
+    if (!teacher) {
+      throw new NotFoundException(`Teacher with ID ${teacherId} not found`);
+    }
+
+    return teacher;
+  }
 }
